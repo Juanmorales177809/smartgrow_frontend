194f5d9fb46a4a05b57af95a7c78794b8de7cdc4
santiagoSuarez219 <santiago8628@gmail.com>
2023-06-08 13:33:24 -0500

    Conexion WebSocket Establecida

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {Hidroponico} from './Hidroponico';
+
+function App() {
+  return (
+    <>
+      <h1>SmartGrow</h1>
+      <Hidroponico/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Hidroponico.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import {io} from "socket.io-client";
+
+const socket = io('http://localhost:8000', {
+    path: '/sockets'
+}); // Connects to socket.io server
+
+export const Hidroponico = () => {
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    useEffect(() => {
+        socket.on('connect', () => {
+          setIsConnected(socket.connected);
+        });
+    
+        socket.on('disconnect', () => {
+          setIsConnected(socket.connected);
+        });
+      }, []);
+    return (<>
+        <h2>status: {isConnected ? 'connected' : 'disconnected'}</h2>
+        <p>Hidroponico</p>
+    </>);
+
+};
