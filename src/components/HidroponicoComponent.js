0e6cf1da20571035c9a9d53fba300468b60f8445
santiagoSuarez219 <santiago8628@gmail.com>
2023-06-26 11:47:32 -0500

    setpoitns al back

--- src/components/HidroponicoComponent.js
+++ src/components/HidroponicoComponent.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Hidroponico.css";
 import { io } from "socket.io-client";
 
 function HidroponicoComponent() {
@@ -23,7 +24,6 @@ function HidroponicoComponent() {
         setSensorTemperaturaData(data.temperatura);
         setSensorECData(data.ec);
         setSensorPHData(data.ph);
-        setSensorECData(data.value);
       } 
     });
   }, []);
@@ -44,17 +44,73 @@ function HidroponicoComponent() {
     }
   };
 
+  const sendStEc = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/st_hidroponic_ec");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendStPh = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/st_hidroponic_ph");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const sendStTemperatura = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/st_hidroponic_temperatura");
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
-    <div>
+  <div class="container">
+    <div class="content-hidro">
       <h2>Hidropónico</h2>
       <p>status: {isConnected ? "connected" : "disconnected"}</p>
       <p>EC: {sensorECData}</p>
       <p>PH: {sensorPHData}</p>
-      <p>Temperatura: {sensorTemperaturaData} </p>
+      <p>Temperatura: {sensorTemperaturaData}</p>
       <button style={{ backgroundColor: buttonColor }} onClick={toogleRele}>
         Rele
       </button>
     </div>
+    <div class="setpoints-section">
+    <h2>Setpoints</h2>
+    <div class="setpoint">
+      <label for="setpoint1">EC:</label>
+      <div class="input-button-container">
+        <input type="text" id="setpoint1" placeholder="Ingrese un valor" />
+        <button class="setpoint-button onClick={sendStEc}">Enviar</button>
+      </div>
+    </div>
+    <div class="setpoint">
+      <label for="setpoint2">PH:</label>
+      <div class="input-button-container">
+        <input type="text" id="setpoint2" placeholder="Ingrese un valor" />
+        <button class="setpoint-button onClick={sendStPh}">Enviar</button>
+      </div>
+    </div>
+    <div class="setpoint">
+      <label for="setpoint3">Temperatura:</label>
+      <div class="input-button-container">
+        <input type="text" id="setpoint3" placeholder="Ingrese un valor" />
+        <button class="setpoint-button" onClick={sendStTemperatura}>Enviar</button>
+      </div>
+    </div>
+  </div>
+  </div>
   );
 }
 
